1359d084f837d7c62a4a80743b1e482d57c49b91
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2023-03-26 19:00:44 -0500

    feat: Second Video, useState the useState into "Users" Component file #20230326b

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Counter } from './components/Counter';
-import { Users } from './components/Users';
+import { Counter } from "./components/Counter";
+import { Users } from "./components/Users";
+import { TimerMaster } from './components/TimerMaster';
 
 function App() {
   return (
     <>
       <h1>React + Typescript</h1>
       <hr />
-
-      <Counter/>
-       <Users/>
+      <h1>useState</h1>
+      <Counter />
+      <Users />
+      <h2>useEffect - useRef</h2>
+      <hr />
+      <TimerMaster/>
     </>
   );
 }
